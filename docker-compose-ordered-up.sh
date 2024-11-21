2a9c50597ba02dec1ba72277b82327252d41b451
Giles Westwood <giles.westwood@gmail.com>
2024-11-21 17:07:49 +0000

    full haproxy and fixed nginx config

--- docker-compose-ordered-up.sh
+++ docker-compose-ordered-up.sh
@@ -7,6 +7,7 @@ docker compose up redis -d
 echo "sleeping 20s to ensure postgres and redis are available"
 sleep 20
 
-
-
+docker compose up web -d
+docker compose up frontend -d
+docker compose up web_proxy -d
 
